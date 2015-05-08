db1e6d2078a815dd04204abb70e7602a1dc03acd
Andrew Chernysh <andrice@i.ua>
2015-05-08 14:10:24 +0300

    Update tictail strategy

--- lib/omniauth/strategies/tictail.rb
+++ lib/omniauth/strategies/tictail.rb
@@ -5,17 +5,18 @@ module OmniAuth
     class Tictail < OmniAuth::Strategies::OAuth2
       option :name, :tictail
 
-      option :client_options, site: 'https://tictail.com/oauth/token'
+      option :client_options, site: 'https://tictail.com'
+      option :provider_ignores_state, true
 
       uid do
-        # Using store url as uid
-        access_token.params['store']['url']
+        access_token.params['store']['id']
       end
 
       info do
         {
           name: access_token.params['store']['name'],
-          email: access_token.params['store']['storekeeper_email']
+          email: access_token.params['store']['storekeeper_email'],
+          url: access_token.params['store']['url']
         }
       end
     end
